a0d1dc0ae960c48285f4d22797d68bfdfe014397
psmuler <zhmuler@gmail.com>
2023-05-03 01:32:14 +0900

    more refurbishment

--- game.js
+++ game.js
@@ -3,13 +3,57 @@ const ctx = canvas.getContext('2d');
 const retryButton = document.getElementById('retryButton');
 const obstacles = [];
 
+let highScore = localStorage.getItem('highScore') || 0;
 let score = 0;
 
+let gameSpeed = 1;
+
+const startScreen = document.getElementById('startScreen');
+const startButton = document.getElementById('startButton');
+
+function showStartScreen() {
+    startScreen.style.display = 'flex';
+}
+
+function hideStartScreen() {
+    startScreen.style.display = 'none';
+}
+startButton.addEventListener('click', () => {
+    hideStartScreen();
+    update();
+});
+
+
 // 画像の読み込み
-const playerImg = new Image();
-playerImg.src = 'src/player.png'; // 主人公の画像ファイルへのパス
+const playerFrames = [
+    'src/player_frame1.png',
+    'src/player_frame2.png'
+];
+
+const playerImages = playerFrames.map((frame) => {
+    const image = new Image();
+    image.src = frame;
+    return image;
+});
+
+let playerAnimationFrame = 0;
+
 const obstacleImg = new Image();
 obstacleImg.src = 'src/obstacle.png'; // 障害物の画像ファイルへのパス
+
+const initialPlayerSpeed = 5;
+const initialObstacleSpeed = 5;
+
+function initializeSpeeds() {
+    player.speed = initialPlayerSpeed;
+    // obstacle.speed = initialObstacleSpeed;
+}
+
+function updateSpeeds() {
+    gameSpeed = 1 + Math.floor(score / 300) * 0.2;
+    player.speed = initialPlayerSpeed * gameSpeed;
+    // obstacle.speed = initialObstacleSpeed * gameSpeed;
+}
 
 const player = {
     x: 50,
@@ -28,7 +72,7 @@ const obstacle = {
 };
 
 function drawPlayer() {
-    ctx.drawImage(playerImg, player.x, player.y, player.width, player.height);
+    ctx.drawImage(playerImages[playerAnimationFrame], player.x, player.y, player.width, player.height);
 }
 function drawObstacle(obstacle) {
     ctx.drawImage(obstacleImg, obstacle.x, obstacle.y, obstacle.width, obstacle.height);
@@ -63,13 +107,12 @@ function createObstacle() {
         y: 320,
         width: 30,
         height: 40,
-        speed: 5,
     };
     obstacles.push(obstacle);
 }
 function updateObstacles() {
     for (let i = 0; i < obstacles.length; i++) {
-        obstacles[i].x -= obstacles[i].speed;
+        obstacles[i].x -= initialObstacleSpeed * gameSpeed
 
         if (obstacles[i].x <= -obstacles[i].width) {
             obstacles.splice(i, 1);
@@ -84,19 +127,32 @@ function displayScore() {
     ctx.fillStyle = 'black';
     ctx.fillText('スコア: ' + score, 10, 30);
 }
+function displayHighScore() {
+    ctx.font = '20px Arial';
+    ctx.fillStyle = 'black';
+    ctx.fillText('ハイスコア: ' + highScore, 10, 60);
+}
+function updateHighScore() {
+    if (score > highScore) {
+        highScore = score;
+        localStorage.setItem('highScore', highScore);
+    }
+}
 function displayGameOver() {
     ctx.font = '30px Arial';
     ctx.fillStyle = 'red';
     ctx.fillText('ゲームオーバー', canvas.width / 2 - 70, canvas.height / 2);
     retryButton.style.display = 'block'; // リトライボタンを表示
 }
-
 function resetGame() {
     player.x = 50;
     player.y = 300;
     player.jumping = false;
     obstacles.length = 0; // 障害物配列をリセット
     retryButton.style.display = 'none'; // リトライボタンを非表示
+    updateHighScore(); // ハイスコアを更新
+    initializeSpeeds();
+    score = 0; // スコアをリセット
     update(); // ゲーム更新を再開
 }
 
@@ -116,29 +172,28 @@ function update() {
         }
     }
 
-    // if (Math.random() < 0.01) {
-    //     createObstacle();
-    // }
-
     if (isColliding(obstacles)) {
         displayGameOver();
         return; // ゲームオーバー時に更新を停止
     }
     else {
         score++;
+        updateSpeeds();
     }
     displayScore();
+    displayHighScore();
 
     drawPlayer();
     updateObstacles();
+    updateAnimationFrame();
     requestAnimationFrame(update);
 }
 function spawnObstacle() {
     createObstacle();
 
     // 最小間隔と最大間隔を設定 (ミリ秒単位)
-    const minInterval = 1000; // 1000ms = 1秒
-    const maxInterval = 3000; // 3000ms = 3秒
+    const minInterval = 1000 / gameSpeed ** 2; // 1000ms = 1秒
+    const maxInterval = 3000 / gameSpeed ** 2; // 3000ms = 3秒
 
     // minInterval から maxInterval までのランダムな間隔を計算
     const randomInterval = Math.random() * (maxInterval - minInterval) + minInterval;
@@ -146,8 +201,9 @@ function spawnObstacle() {
     // 次の障害物生成までのタイマーを設定
     setTimeout(spawnObstacle, randomInterval);
 }
+function updateAnimationFrame() {
+    playerAnimationFrame = Math.floor(score / 20 * gameSpeed) % playerImages.length;
+}
 
 // ゲーム開始時に最初の障害物生成タイマーを設定
 setTimeout(spawnObstacle, 2000); // 最初の障害物は2秒後に生成
-
-update();
